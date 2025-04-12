eac0fa01aee0922915ee4bbc8daf3408a642647b
Sirapop Sorntad <Sirapop0122@gmail.com>
2025-04-12 15:51:59 +0700

    Add Footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,22 @@
 'use client';
 
 import Nav from '../component/nav';
-
+import Footer from '../component/footer';
 
 export default function Timeline() {
 
   return (
-    <div >
+    <div className="min-h-screen flex flex-col">
       <Nav />
-      <div className='text-7xl flex justify-center mt-10 text-white font-bold'> 
+
+      {/* เนื้อหาหลัก */}
+      <div className="flex-grow text-7xl flex justify-center mt-10 text-white font-bold">
         Hello Timeline
       </div>
+
+      <Footer />
     </div>
+
   );
 }
 
