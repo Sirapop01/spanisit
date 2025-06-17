906e3105745e4befe87ba1728fcb0498af2c7988
Sirapop Sorntad <Sirapop0122@gmail.com>
2025-06-17 17:40:31 +0700

    Fix icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
+
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,11 +16,6 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "Student Council",
-  icons: {
-    icon: "/assets/spa_logo.png",
-    shortcut: "/assets/spa_logo.png",
-    apple: "/assets/spa_logo.png",
-  },
   description: "About Student Council",
 };
 
